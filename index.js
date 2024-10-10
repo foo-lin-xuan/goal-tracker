f5f46e580929c0e75ba13d645c6cf91f0dc8c3c0
toonuaaa <149750175+toonuaaa@users.noreply.github.com>
2024-10-10 11:22:26 +0800

    added CRUD operations

--- index.js
+++ index.js
@@ -1,4 +1,8 @@
 const express = require("express");
+const ejs = require("ejs");
+const path = require("path");
+const methodOverride = require("method-override");
+
 const mongoose = require("mongoose");
 const Task = require("./models/task");
 
@@ -13,15 +17,39 @@ mongoose
 
 const app = express();
 
-// For testing
+app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "views"));
 
-// app.get("/testTask", async (req, res) => {
-//   const t = new Task({
-//     name: "Work on 1 personal project for 2 hours",
-//   });
-//   await t.save();
-//   res.send(t);
-// });
+app.use(express.json()); // for parsing application/json
+app.use(express.urlencoded({ extended: true })); // for parsing application/x-www-form-urlencoded
+app.use(methodOverride("_method")); // override with POST having ?_method=DELETE
+
+app.get("/tasks", async (req, res) => {
+  const tasks = await Task.find();
+  res.render("tasks/index", { tasks });
+});
+
+app.post("/tasks", async (req, res) => {
+  await Task.create(req.body.task);
+  res.redirect("/tasks");
+});
+
+app.get("/tasks/:id/edit", async (req, res) => {
+  const task = await Task.findById(req.params.id);
+  res.render("tasks/edit", { task });
+});
+
+app.patch("/tasks/:id", async (req, res) => {
+  const task = await Task.findByIdAndUpdate(req.params.id, {
+    ...req.body.task,
+  });
+  res.redirect("/tasks");
+});
+
+app.delete("/tasks/:id", async (req, res) => {
+  await Task.findByIdAndDelete(req.params.id);
+  res.redirect("/tasks");
+});
 
 app.get("/", (req, res) => {
   res.send("This is a goal tracker app");
